6b68c6d7ada0095fa2522680320d096642b2012a
bolisnsk <xognssnsk@gmail.com>
2023-11-08 01:12:15 +0900

    메인 화면 네비게이션 바 추가 및 레이아웃 수정

--- about.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.btn');
-const menu = document.querySelector('.nav_menu');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
