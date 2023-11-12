118d6b61b17add4d8c650d1ac1ca04d943c730f1
bolisnsk <xognssnsk@gmail.com>
2023-11-13 03:46:28 +0900

    각 메뉴별 리스트 페이지 html 완성

--- js/about.js
+++ js/about.js
@@ -5,3 +5,5 @@ toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
 
+const list = document.querySelector('.list');
+const resAd = document.querySelector('.itemAd');
