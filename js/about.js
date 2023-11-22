5f6127e4e874730aaa242f651cdfd464fcc205b6
bolisnsk <xognssnsk@gmail.com>
2023-11-23 04:14:49 +0900

    네비바 애니메이션 추가

--- js/about.js
+++ js/about.js
@@ -1,9 +1,28 @@
-const toggleBtn = document.querySelector('.nav_toggleBtn');
-const menu = document.querySelector('.nav_menu');
+const foodName = document.querySelector('.foodName');
+const foodAd = document.querySelector('.foodAd');
+const time = document.querySelector('.time');
+const comment = document.querySelector('.comment');
+const menu1 = document.querySelector('.menu1');
+const menu2 = document.querySelector('.menu2');
+const menu3 = document.querySelector('.menu3');
+const imgs = document.querySelector('.imgs');
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active')
-})
+temp = location.href.split("?");
+data = temp[1].split(",");
+value = data[0];
+console.log(value);
+const foodId = Number(value);
+// 해당 데이터 가져오기 콜백함수가 숫자라서 Number 써줘야하는듯?
+const targetData = restaurant.find((data) => data.id === foodId);
+console.log(targetData);
 
-const list = document.querySelector('.list');
-const resAd = document.querySelector('.itemAd');
+foodName.innerText = targetData.name;
+foodAd.innerText = targetData.addres;
+comment.innerText = targetData.comment;
+time.innerText = targetData.time;
+menu1.innerText = targetData.menu1;
+menu2.innerText = targetData.menu2;
+menu3.innerText = targetData.menu3;
+imgs.src = "../imges/" + targetData.img;
+
+
---
+++ js/index.js
@@ -0,0 +1,24 @@
+const toggleBtn = document.querySelector('.nav_toggleBtn');
+const menu = document.querySelector('.nav_menu');
+const nav = document.querySelector('.navbar');
+const inner = document.querySelector('.inner');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+
+let scrollNum = 0;
+let documentHeight = 0;
+
+window.addEventListener("scroll", () => {
+    scrollNum = window.scrollY;
+    documentHeight = document.body.scrollHeight - window.innerHeight;
+    console.log(document.body.scrollHeight);
+    if(scrollNum >= 10){ 
+        nav.classList.add("fixed");
+    } else {
+        //화면에서 header, title 영역이 보일때
+        nav.classList.remove("fixed");
+        inner.style.top = -scrollNum / 10 + "px";
+    }
+});
